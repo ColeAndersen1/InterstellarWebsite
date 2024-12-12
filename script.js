46cf682e4fe65c4fd2964169b85f72155005b442
ColeAndersen1 <161650405+ColeAndersen1@users.noreply.github.com>
2024-12-12 10:53:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Set the date we're counting down to
-    var countDownDate = new Date("Aug 1, 2025 14:00:00").getTime();
+    var countDownDate = new Date("May 1, 2025 14:00:00").getTime();
 
     // Update the countdown every 1 second
     var x = setInterval(function() {
